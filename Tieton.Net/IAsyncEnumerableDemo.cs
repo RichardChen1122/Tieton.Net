625a20b554eed7eaa3ef9968a72db38cd04d8e01
Richard Chen <junbchen@microsoft.com>
2023-09-20 10:49:43 +0800

    Add project files.

---
+++ Tieton.Net/IAsyncEnumerableDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tieton.Net
+{
+    public class IAsyncEnumerableDemo
+    {
+        public static async IAsyncEnumerable<string> ReadAllLines(string file)
+        {
+            using (var fs = File.OpenRead(file))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    while (true)
+                    {
+                        string line = await sr.ReadLineAsync();
+
+                        await Task.Delay(100);
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        yield return line;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tieton.Net/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.Common;
+
+namespace Tieton.Net
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            await WouldTimeoutInTheMiddelOfIteration();
+            Console.ReadKey();
+        }
+
+
+        static async Task WouldNotTimeout()
+        {
+            var lines = IAsyncEnumerableDemo.ReadAllLines(@"D:\feature.json");
+
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(1));
+
+            await foreach (var line in lines.WithCancellation(cts.Token))
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+
+        static async Task WouldTimeoutInTheMiddelOfIteration()
+        {
+            var lines = IAsyncEnumerableDemo.ReadAllLines(@"D:\feature.json");
+
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(1));
+
+            var t = lines.SelectAwaitWithCancellation(async (s, c) =>
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(5), c);
+                //await Task.Delay(TimeSpan.FromMilliseconds(100), c);
+                return ValueTask.FromResult(s);
+            });
+
+            await foreach (var line in t.WithCancellation(cts.Token))
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
